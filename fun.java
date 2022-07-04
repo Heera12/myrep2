60dad3b6984b482d91815eeb670e3f7b4e6cb180
Shaik Nazeem Heera <nazeemshaik.12@gmail.com>
2022-07-04 10:03:32 +0530

    Changes Updated!

--- fun.java
+++ fun.java
@@ -1,7 +1,21 @@
-public static void fun() {
-    return 1+2;
-    System.out.println("Addition done") {
-        
-    }
-    
+public static void main fun()
+{
+int a;
+int b;
+public void add() {
+    System.out.println("Addition:")
+    return a+b;
+}
+public void sub() {
+    System.out.println("Subtraction:")
+    return a-b;
+}
+public void mul() {
+    System.out.println("Multiplication:")
+    return a*b;
+}
+public void add() {
+    System.out.println("Divide:")
+    return a/b;
+}
 }
